4ee50c2e43edabcb33f1f58f286f18ce64bcebf3
Marcel Lamm <atomy@jackinpoint.net>
2018-02-17 17:17:41 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# Overview
+This is a script for notifications to [Discord|https://discordapp.com/] issued from [Zabbix|https://www.zabbix.com/]
+It uses webhooks.
+
+# Installation
+- add the *discord.sh* script to your zabbix alertscripts directory
+- configure your custom alertscript in zabbix
+- ???
+- profit
+
+## Howto setup webhook for discord
+https://support.discordapp.com/hc/en-us/articles/228383668-Intro-to-Webhooks
+
+## Configure custom zabbix alertscript
+https://www.zabbix.com/documentation/3.4/manual/config/notifications/media/script
+
+Just go with the 3 macros:
+{ALERT.SENDTO}, {ALERT.SUBJECT} and {ALERT.MESSAGE}
+
+I build a regexp into the script to detect if it's either a *PROBLEM* or *OK* (never got things like {EVENT.VALUE} working)
+
+## Example webhook for discord
+```$ curl -X POST --data '{ "embeds": [{"title": "<NOTIFICATION TITLE>", "url": "<NOTIFICATION URL>", "description": "<NOTIFICATION BODY>", "type": "link", "thumbnail": {"url": "https://storage.googleapis.com/material-design/publish/material_v_12/assets/0Bx4BSt6jniD7dFBUNHdrY05jQ3c/style-logos-product-intro-definition.png"}}] }' -H "Content-Type: application/json" https://discordapp.com/api/webhooks/xxx/yyyy```
+
+## Discord notification in action
+![Image of Discord-Notifcation(https://github.com/atomy/zabbix-discord/doc/discord-zabbix-notification.png)
---
+++ discord.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+set -e
+
+IN_TO=$1
+IN_SUBJECT=$2
+IN_BODY=$3
+
+json_escape() {
+    printf '%s' $1 | python -c 'import json,sys; print(json.dumps(sys.stdin.read()))'
+}
+
+CURL_TO=$(json_escape "${IN_TO}")
+CURL_SUBJECT=$(json_escape "${IN_SUBJECT}")
+CURL_BODY=$(json_escape "${IN_BODY}")
+
+if [[ "${IN_SUBJECT}" =~ ^PROBLEM:.*$ ]] ; then
+    echo "We received a PROBLEM!"
+    HTTP_RESPONSE=`curl --write-out %{http_code} --silent --output /dev/null -X POST --data '{ "embeds": [{"title": '"${CURL_SUBJECT}"', "url": "https://zabbix.your-domain-bleh.foo/", "description": '"${CURL_BODY}"', "type": "link", "thumbnail": {"url": "http://icons.iconarchive.com/icons/paomedia/small-n-flat/96/sign-warning-icon.png"}}] }' -H "Content-Type: application/json" https://discordapp.com/api/webhooks/xxx/yyy`
+elif [[ "${IN_SUBJECT}" =~ ^OK:.*$ ]] ; then
+    echo "We received OKAY!"
+    HTTP_RESPONSE=`curl --write-out %{http_code} --silent --output /dev/null -X POST --data '{ "embeds": [{"title": '"${CURL_SUBJECT}"', "url": "https://zabbix.your-domain-bleh.foo/", "description": '"${CURL_BODY}"', "type": "link", "thumbnail": {"url": "http://icons.iconarchive.com/icons/paomedia/small-n-flat/256/sign-check-icon.png"}}] }' -H "Content-Type: application/json" https://discordapp.com/api/webhooks/xxx/yyy`
+else
+    echo "Dunno what this was: ${IN_SUBJECT}"
+    HTTP_RESPONSE=`curl --write-out %{http_code} --silent --output /dev/null -X POST --data '{ "embeds": [{"title": '"${CURL_SUBJECT}"', "url": "https://zabbix.your-domain-bleh.foo/", "description": '"${CURL_BODY}"', "type": "link", "thumbnail": {"url": "http://icons.iconarchive.com/icons/paomedia/small-n-flat/256/sign-question-icon.png"}}] }' -H "Content-Type: application/json" https://discordapp.com/api/webhooks/xxx/yyy`
+fi
+
+if [[ "${HTTP_RESPONSE}" != "204" ]] ; then
+    echo "Received HTTP-Code: ${HTTP_RESPONSE}"
+    exit 1
+else
+    exit 0
+fi
